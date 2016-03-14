9c047c912f603d48b03d6c376bb5b38eb799cf70
bhavintandel <bhautandel@yahoo.com.com>
2016-03-14 22:49:38 +0000

    Added method to count specific word

--- src/WordCountProgram/WordCount.java
+++ src/WordCountProgram/WordCount.java
@@ -1,7 +1,13 @@
 package WordCountProgram;
 
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * 
@@ -20,9 +26,34 @@ public class WordCount {
 		return file;
 	}
 	
+	public int readFile(FileReader file, String word){
+        Pattern p = Pattern.compile("\\b" + word + "\\b", Pattern.CASE_INSENSITIVE);
+        BufferedReader br = new BufferedReader(file);
+        String currentLine = null;
+        String[] arrayList = null; 
+        int count = -1;
+                
+    try{
+        while ((currentLine = br.readLine()) != null) {
+            arrayList = currentLine.split("[^a-zA-Z]");
+            for(String str : arrayList){
+                Matcher M = p.matcher(str);
+                if (M.find()){
+                    count++;
+                }
+            }
+        }
+    } catch(Exception e) {}
+
+    return count;
+    }
+	
+	
 	public void main(String[] args){
 		String fileName = "F:/Cloudwick/java_Cloudwick/gutenberg/war&peace.txt";
 		FileReader file = inputFile(fileName);
+		int count = readFile(file, "the");
+		
 	}
 
 }
